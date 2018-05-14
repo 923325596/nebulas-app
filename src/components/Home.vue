02cd9a58bf27ccb6bf46522dac49257ac0a47f02
yany2 <yuan.yan@dianrong.com>
2018-05-14 17:37:25 +0800

    blacklist

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="layout">
-    <mu-toast v-if="toast" :message="message" @close="hideToast"/>
     <div class="header">
       <div class="title">
         天眼
@@ -8,16 +7,34 @@
       <div class="logo">
         <img src="../assets/nebulasx60.png" alt="">
       </div>
+      <div class="right">
+        <a href="https://github.com/YanYuanFE/nebulas-app">
+          <mu-icon value="code" color="#FFF" :size="32"/>
+        </a>
+      </div>
     </div>
     <div class="content">
+      <div class="net">
+        <mu-dropDown-menu :value="net" @change="handleChange">
+          <mu-menu-item
+            :value="item.value"
+            :title="item.label"
+            :key="item.value"
+            v-for="item in netArr"/>
+        </mu-dropDown-menu>
+      </div>
       <div class="body">
         <div class="left">
           <div class="row">
-            <mu-text-field v-model="value" hintText="输入你想查找的企业"/>
+            <mu-text-field
+              v-model="value"
+              hintText="输入你想查找的企业"
+              :errorText="value ? '' : errorText"
+              :maxLength="30"/>
             <mu-raised-button label="搜索" class="demo-raised-button" primary @click="search"/>
           </div>
 
-          <div class="no-result" v-if="result">
+          <div class="no-result" v-if="!result">
             <div class="row">
               <span>
                 输入的企业没有记录！
@@ -25,18 +42,40 @@
               <mu-raised-button label="添加" class="demo-raised-button" primary @click="add"/>
             </div>
             <div class="row" v-if="visible">
-              <mu-text-field v-model="addValue" hintText="描述企业信息" multiLine :rows="3" :rowsMax="6"/>
+              <mu-text-field
+                v-model="content"
+                hintText="描述企业信息"
+                multiLine
+                :rows="3"
+                :rowsMax="6"
+                :maxLength="500"
+                :errorText="content ? '' : errorText"/>
               <mu-raised-button label="提交" class="demo-raised-button" primary @click="submit"/>
             </div>
           </div>
+          <div class="result" v-if="result && result.key">
+            <div class="name">公司名称： {{result.key}}</div>
+            <div class="description">公司信息： {{result.value}}</div>
+            <div class="author">提交人： {{result.author}}</div>
+          </div>
+          <div class="extension" v-if="!hasExtension">
+            检测到你尚未安装星云钱包<a href="https://github.com/ChengOrangeJu/WebExtensionWallet">WebExtensionWallet</a>扩展，请先安装然后使用。
+          </div>
         </div>
         <div class="right">
-
+          <mu-card>
+            <mu-card-header title="星云" subTitle="天眼">
+            </mu-card-header>
+            <mu-card-text>
+              天眼是基于星云链开发的DApp，通过区块链去中心化的透明机制，用于求职者爆料虚假企业、黑心企业，
+              用户通过搜索企业名称查询是否在区块链数据库中，未查询到信息则可以添加相关描述提交相关信息。
+            </mu-card-text>
+          </mu-card>
         </div>
       </div>
     </div>
     <div class="footer">
-      Muse-UI ©2017 Created by Muse-UI
+      天眼 ©2018 Created by Nebulas
     </div>
 
   </div>
@@ -48,112 +87,130 @@ import Nebulas from 'nebulas';
 const Account = Nebulas.Account;
 const neb = new Nebulas.Neb();
 const nebPay = new NebPay();
-const dappAddress = 'n1utupNggY4GV4JynFX45kURgtSCv5xhpek';
+// const dappAddress = 'n1zo1HT9cbJTYKhsXM7jfpC8Hh8uiUuP79T';
+// 'n1utupNggY4GV4JynFX45kURgtSCv5xhpek';
 // cd $GOPATH/src/github.com/nebulasio/go-nebulas
 // ./neb -c conf/default/config.conf
+// hash 666befeaabe0dcc149aa9f24ab5ba4ecd0f7b94703b2d89679279baf4d8eda3f
 
 export default {
   data () {
     return {
       value: '',
-      addValue: '',
+      content: '',
       result: true,
-      message: '',
-      toast: false,
       visible: false,
-    }
+      errorText: '',
+      net: 'https://mainnet.nebulas.io',
+      dappAddress: 'n1emrHvHnDAHeeqQjCXhCUfgiDztxDJupa5',
+      netArr: [{
+        label: 'TestNet',
+        value: 'https://testnet.nebulas.io',
+        address: 'n1zo1HT9cbJTYKhsXM7jfpC8Hh8uiUuP79T'
+      }, {
+        label: 'MainNet',
+        value: 'https://mainnet.nebulas.io',
+        address: 'n1emrHvHnDAHeeqQjCXhCUfgiDztxDJupa5'
+      }],
+      hasExtension: webExtensionWallet || false /*  global webExtensionWallet   */
+    };
   },
-  created() {
+  created () {
     this.checkNebpay();
-    this.switchNet();
+    this.switchNet(this.net);
   },
   methods: {
-    checkNebpay() {
-      console.log("check nebpay")
-      try{
-          console.log(NebPay);
-
-      }catch(e){
-          console.log("Extension wallet is not installed, please install it first.")
-          
+    checkNebpay () {
+      console.log('check nebpay');
+      try {
+        console.log(NebPay);
+      } catch (e) {
+        console.log('Extension wallet is not installed, please install it first.');
       }
     },
-    switchNet() {
-      // neb.setRequest(new Nebulas.HttpRequest("localhost:8685"));
-      neb.setRequest(new Nebulas.HttpRequest("https://testnet.nebulas.io"));
+    handleChange (value) {
+      console.log(value);
+      this.net = value;
+      const currentIndex = this.netArr.findIndex((item) => item.value === value);
+      this.dappAddress = this.netArr[currentIndex].address;
+      this.switchNet(value);
     },
-    search() {
+    switchNet (value) {
+      // neb.setRequest(new Nebulas.HttpRequest("localhost:8685"));
+      console.log(value);
+      neb.setRequest(new Nebulas.HttpRequest(value));
+    },
+    search () {
+      if (!this.value && this.value !== 0) {
+        this.errorText = '请输入';
+        return;
+      };
       const from = Account.NewAccount().getAddressString();
-      const value = "0";
-      const nonce = "0";
-      const gas_price = "1000000";
-      const gas_limit = "2000000";
-      const callFunc = "get";
+      const value = '0';
+      const nonce = '0';
+      const gasPrice = '1000000';
+      const gasLimit = '2000000';
+      const callFunc = 'get';
       const callArgs = JSON.stringify([this.value]);
       const contract = {
         function: callFunc,
-        args: callArgs,
-      }
-      neb.api.call(from, dappAddress, value, nonce, gas_price, gas_limit, contract).then((res) => {
-        this.afterSearch(res)
-      }).catch(err => console.log("error:" + err))
+        args: callArgs
+      };
+      neb.api.call(from, this.dappAddress, value, nonce, gasPrice, gasLimit, contract).then((res) => {
+        this.afterSearch(res);
+      }).catch(err => console.log(`error:${err}`));
     },
-    afterSearch(res) {
+    afterSearch (res) {
       console.log(res);
       let result = res.result;
       if (result === 'null') {
-        this.showToast(result.execute_err);
+        this.result = false;
+        // this.showToast(res.execute_err);
+        return;
       }
-      // result = JSON.parse(result);
-      // if (!!result.key) {
-      //   this.result = result;
-      // } else {
-      //   this.result = null;
-      // }
+      result = JSON.parse(result);
+      if (result.key) {
+        this.result = result;
+      } else {
+        this.result = null;
+      }
     },
-    add() {
+    add () {
       this.visible = true;
     },
-    submit() {
-      const to = dappAddress;
-      const value = "0";
-      const callFunc = "save";
-      const callArgs = JSON.stringify([this.value, this.add]);
-      
+    submit () {
+      if (!this.value || !this.content) {
+        this.errorText = '请输入';
+        return;
+      }
+      const to = this.dappAddress;
+      const value = '0';
+      const callFunc = 'save';
+      const callArgs = JSON.stringify([this.value, this.content]);
       this.serialNumber = nebPay.call(to, value, callFunc, callArgs, {
         listener: this.cbPush
-      })
+      });
 
       this.timer = setInterval(() => {
         this.queryInterval();
       }, 5000);
     },
-    cbPush(res) {
-      console.log('res of push:'+JSON.stringify(res));
+    cbPush (res) {
+      console.log(`res of push:${JSON.stringify(res)}`);
     },
-    queryInterval() {
+    queryInterval () {
       nebPay.queryPayInfo(this.serialNumber)
         .then(res => {
-          console.log("tx result: "+ res);
+          console.log(`tx result: ${res}`);
           const resObj = JSON.parse(res);
-          if(resObj.code === 0) {
+          if (resObj.code === 0) {
             alert(`set ${this.value} succeed!`);
             clearInterval(this.timer);
           }
-        })
-    },
-    showToast (message) {
-      this.toast = true
-      this.message = message
-      if (this.toastTimer) clearTimeout(this.toastTimer)
-      this.toastTimer = setTimeout(() => { this.toast = false }, 2000)
-    },
-    hideToast () {
-      this.toast = false
-      if (this.toastTimer) clearTimeout(this.toastTimer)
+        });
     }
-  },
-}
+  }
+};
 </script>
 <style scoped>
 .layout{
@@ -174,6 +231,11 @@ export default {
 .logo {
   display: inline-block;
   vertical-align: middle;
+}
+
+.right {
+  display: inline-block;
+  float: right;
 }
 
 .logo img {
@@ -212,6 +274,7 @@ export default {
 
 .left, .right {
   flex: 1;
+  padding: 20px;
 }
 
 .footer{
--- src/smart/blacklist.js
+++ src/smart/blacklist.js
@@ -1,113 +1,70 @@
+'use strict';
 
-class Company {
-  constructor(obj) {
+var Company = function (company) {
+  if (company) {
+    var obj = JSON.parse(company);
     this.key = obj.key;
     this.value = obj.value;
     this.author = obj.author;
+  } else {
+    this.key = '';
+    this.author = '';
+    this.value = '';
   }
-}
-
-class BlackList {
-  constructor({key, value, author}) {
-    this.key = key;
-    this.value = value;
-    this.author = author;
-    LocalContractStorage.defineMapProperty(this, "blacklist", {
-      parse: (obj) => {
-        return new Company(obj);
-      },
-      stringify: (obj) => {
-        return obj.toString();
-      }
-    })
-  }
-
-  init() {
-
-  }
-
-  save(key, value) {
-    const from = Blockchain.transaction.from;
-    const item = new Company({from, key, value});
-    this.blacklist.put(key, item);
-  }
-
-  get(key) {
-    return this.blacklist.get(key);
-  }
-}
-
-module.exports = BlackList;
-
-
-"use strict";
-
-var Company = function(text) {
-	if (text) {
-		var obj = JSON.parse(text);
-		this.key = obj.key;
-		this.value = obj.value;
-		this.author = obj.author;
-	} else {
-	    this.key = "";
-	    this.author = "";
-	    this.value = "";
-	}
 };
 
 Company.prototype = {
-	toString: function () {
-		return JSON.stringify(this);
-	}
+  toString: function () {
+    return JSON.stringify(this);
+  }
 };
 
 var BlackList = function () {
-    LocalContractStorage.defineMapProperty(this, "blacklist", {
-        parse: function (text) {
-            return new Company(text);
-        },
-        stringify: function (o) {
-            return o.toString();
-        }
-    });
+  LocalContractStorage.defineMapProperty(this, 'blacklist', {
+    parse: function (company) {
+      return new Company(company);
+    },
+    stringify: function (obj) {
+      return obj.toString();
+    }
+  });
 };
 
 BlackList.prototype = {
-    init: function () {
-        // todo
-    },
+  init: function () {
+    // todo
+  },
 
-    save: function (key, value) {
-
-        key = key.trim();
-        value = value.trim();
-        if (key === "" || value === ""){
-            throw new Error("empty key / value");
-        }
-        if (value.length > 64 || key.length > 64){
-            throw new Error("key / value exceed limit length")
-        }
-
-        var from = Blockchain.transaction.from;
-        var item = this.repo.get(key);
-        if (item){
-            throw new Error("value has been occupied");
-        }
-
-        item = new Company();
-        item.author = from;
-        item.key = key;
-        item.value = value;
-
-        this.blacklist.put(key, item);
-    },
-
-    get: function (key) {
-        key = key.trim();
-        if ( key === "" ) {
-            throw new Error("empty key")
-        }
-        return this.blacklist.get(key);
+  save: function (key, value) {
+    key = key.trim();
+    value = value.trim();
+    if (key === '' || value === '') {
+      throw new Error('empty key / value');
     }
+    if (value.length > 30 || key.length > 500) {
+      throw new Error('key / value exceed limit length');
+    }
+
+    var from = Blockchain.transaction.from;
+    var item = this.blacklist.get(key);
+    if (item) {
+      throw new Error('value has been occupied');
+    }
+
+    item = new Company();
+    item.author = from;
+    item.key = key;
+    item.value = value;
+
+    this.blacklist.put(key, item);
+  },
+
+  get: function (key) {
+    key = key.trim();
+    if (key === '') {
+      throw new Error('empty key');
+    }
+    return this.blacklist.get(key);
+  }
 };
 module.exports = BlackList;
